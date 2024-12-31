1e351feb9f636a04c38a8e8e571f8b4514dd323a
Raul <raulvaliturbide@gmail.com>
2024-12-31 11:50:32 +0100

    Update Jugador_Clase.java

---
+++ Jugador_Clase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sendadelalquimista;
+
+/**
+ * Crearemos distintas clases con distintos puntos de vida y ataque de base que puedan portar diferentes 
+ * tipos de items excluyentes entre si ej:un espadon será propio de una clase y solo de esa clase
+ * excluyendo a las demás
+ * @author baske
+ */
+public class Jugador_Clase {
+    
+}
