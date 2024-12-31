83fc5e34cb801a90b4c7005c9aca0418f0f4b549
Raul <raulvaliturbide@gmail.com>
2024-12-31 11:50:00 +0100

    Update SendaDelAlquimista.java

---
+++ SendaDelAlquimista.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sendadelalquimista;
+
+/**
+ *El objetivo del juego es crear un mapa general donde el jugador vaya recorriendo su propio camino con diferentes tipos de encuentros aleatorios hasta llegar al final
+ *consiguiendo diferentes objetos y mejoras por el camino 
+ * @author baske
+ */
+public class SendaDelAlquimista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
