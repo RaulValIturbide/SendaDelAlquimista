760aad11db41f15e1c5f374ca867d7a78a674d48
Raul <raulvaliturbide@gmail.com>
2025-01-01 21:15:47 +0100

    Update SendaDelAlquimista.java

--- SendaDelAlquimista.java
+++ SendaDelAlquimista.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sendadelalquimista;
 
 /**
- *El objetivo del juego es crear un mapa general donde el jugador vaya recorriendo su propio camino con diferentes tipos de encuentros aleatorios hasta llegar al final
- *consiguiendo diferentes objetos y mejoras por el camino 
+ * El objetivo del juego es crear un mapa general donde el jugador vaya
+ * recorriendo su propio camino con diferentes tipos de encuentros aleatorios
+ * hasta llegar al final consiguiendo diferentes objetos y mejoras por el camino
+ *
  * @author baske
  */
 public class SendaDelAlquimista {
@@ -15,7 +14,31 @@ public class SendaDelAlquimista {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Encuentro encuentro = new Encuentro();
+        Jugador alquimista = new Jugador(100, 10, 100, 100, "Cazador", true);
+        Enemigo[] aEnemigo = new Enemigo[5];
+        enemigo(aEnemigo);
+        
+        
+        
+     
+
     }
-    
+
+    /**
+     * Un metodo que genera los enemigos que apareceran en el juego creado aqui
+     * para una mayor limpieza del programa
+     *
+     * @param aEnemigo El objeto que se desea crear, en este caso los enemigos.
+     * @return Devuelve el array con los objetos enemigo ya creados.
+     */
+    private static Enemigo[] enemigo(Enemigo[] aEnemigo) {
+        aEnemigo[0] = new Enemigo(100, 10, 10, 10, "Goblin", true);
+        aEnemigo[1] = new Enemigo(100, 10, 10, 10, "Ogro", true);
+        aEnemigo[2] = new Enemigo(100, 10, 10, 10, "Orco", true);
+        aEnemigo[3] = new Enemigo(100, 10, 10, 10, "Explorador", true);
+        aEnemigo[4] = new Enemigo(100, 10, 10, 10, "Esqueleto", true);
+        return aEnemigo;
+    }
+
 }
