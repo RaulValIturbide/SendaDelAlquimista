a181301430afc9ca930029ff07896285744375aa
Raul <raulvaliturbide@gmail.com>
2025-01-02 01:36:18 +0100

    Update NOTAS.java

---
+++ NOTAS.java
@@ -0,0 +1,29 @@
+
+package sendadelalquimista;
+
+public class NOTAS {
+    
+    
+    
+    
+    /*
+    02/12/2025
+    -Arreglar el problema en el que un arma tiene siempre un daño determinado
+    -Crear una clase pociones para mezclar y crear habilidades de alquimia
+    -Crear el mapa
+    -
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    */
+    
+}
