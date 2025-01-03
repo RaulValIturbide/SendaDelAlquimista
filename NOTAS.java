e24123ddea6effddf7f52f36e2e972354cc7e2ee
Raul <raulvaliturbide@gmail.com>
2025-01-03 14:13:03 +0100

    Update NOTAS.java

--- NOTAS.java
+++ NOTAS.java
@@ -1,17 +1,31 @@
 
 package sendadelalquimista;
 
-public class NOTAS {
+public class Notas {
     
+    /*Problemas a arreglar*/
+    
+    
+    
+    
+    /*Problemas arreglados
+        -No puedo agregar el objeto alquimia en la ronda 
+    
+    
+    */
     
     
     
     /*
-    02/12/2025
+    02/01/2025
     -Arreglar el problema en el que un arma tiene siempre un daño determinado
     -Crear una clase pociones para mezclar y crear habilidades de alquimia
     -Crear el mapa
-    -
+    
+    03/01/2025
+    -Creo que he solucionado el problema de llevar las pociones dentro de cada batalla introduciendo el array aAlquimia[]en los metodos para que el jugador tenga acceso (o no) a 
+    todos ellos, me parece que crearé un boolean que permita activar/desactivar su uso.
+    
     
     
     
